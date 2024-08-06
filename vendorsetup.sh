7139d1b1addeae574b3a1c62173e850ab4d23a99
Virendra Carpenter <66617140+virendracarpenter@users.noreply.github.com>
2024-08-06 10:53:24 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
-git clone --depth=1 https://github.com/selfmusing/vendor_xiaomi_violet.git -b udc vendor/xiaomi/violet
-git clone --depth=1 https://github.com/selfmusing/kernel_xiaomi_violet.git -b main kernel/xiaomi/violet
+git clone --depth=1 https://github.com/kibria5/android_vendor_xiaomi_violet -b udc vendor/xiaomi/violet
+git clone --depth=1 https://github.com/kibria5/android_kernel_xiaomi_violet.git -b udc kernel/xiaomi/violet
 git clone --depth=1 https://gitlab.com/CuriousNom/chaste_vendor-xiaomi-firmware-violet.git -b 14.0 vendor/xiaomi-firmware/violet
 git clone --depth=1 https://github.com/psionicprjkt/android_prebuilts_clang_host_linux-x86_clang-r522817 prebuilts/clang/host/linux-x86/clang-r522817 && git lfs fetch && git lfs install && git lfs checkout
 git clone --depth=1 https://gitlab.com/kibria5/android_vendor_MiuiCamera -b 14 vendor/MiuiCamera
